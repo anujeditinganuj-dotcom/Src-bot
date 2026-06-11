06f7888acb3a0fb6806b1f668b8aee27dcd8640b
anujeditinganuj-dotcom <anujeditinganuj@gmail.com>
2026-06-11 14:32:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ import os
 # Telegram Bot Credentials
 # ==============================
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8831502190:AAGr8UlSIo_uauaHmi_U7Y2hCJba-DvM--8")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8015464564:AAFe6QCyYpfSWPGbwih_u_XejaDLcho1KOI")
 API_ID = int(os.environ.get("API_ID", "37476811"))
 API_HASH = os.environ.get("API_HASH", "7aa60670b871050820086c6267371ee6")
 
